d412a59738bbcf5a0ab188e7802e3e8bbfddeb14
Sirui Zhang <siruizhangdev@gmail.com>
2022-12-04 23:03:43 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server', // specific to docker-compose
+  port: '6379' // default port number
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    client.set('visits', parseInt(visits) + 1);
+    res.send(`Number of visits is ${visits}`);
+  });
+});
+
+app.listen(8081, () => {
+  console.log("Listening on port 8081");
+});
